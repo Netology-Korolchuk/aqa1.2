223c2a41dacb1cf61fd345ad1bfd925c2abbc23b
Korolchuk Vyacheslav <post@a4st.ru>
2019-10-21 15:45:53 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Задача №1 - Настройка CI
+[![Build status](https://ci.appveyor.com/api/projects/status/8ey5381knf6mp6vo?svg=true)](https://ci.appveyor.com/project/Netology-Korolchuk/aqa1-2)
---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology.service;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class CashbackHackServiceTest {
+
+/*
+    @Test
+    void shouldReturn1000IfAmountIs0() {
+        CashbackHackService cashback = new CashbackHackService();
+        int amount = 0;
+        int actual = cashback.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+*/
+
+    @Test
+    void shouldReturn1IfAmountIs999() {
+        CashbackHackService cashback = new CashbackHackService();
+        int amount = 999;
+        int actual = cashback.remain(amount);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturn0IfAmountIs1000() {
+        CashbackHackService cashback = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashback.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+}
